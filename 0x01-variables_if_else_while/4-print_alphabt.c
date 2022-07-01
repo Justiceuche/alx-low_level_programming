d28f875f009b7cbcb1f48d2a80ad59f5d9ebaefe
Justiceuchej <Justice404050@gmail.com>
2022-07-01 16:35:01 -0700

    my co

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,22 +7,16 @@
 int main(void)
 {
 char c = 'a';
+
 while (c <= 'z')
 {
 if (c != 'e' && c != 'q')
 {
 putchar(c);
-}
+}																}
 c++;
 }
 
 putchar('\n');
 return (0);
-}
-
-
-putchar('\n');
-
-return (0);
-
 }
